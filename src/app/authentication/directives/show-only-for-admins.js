19586933b96977d963c3224e5004829b29ce9ae0
negreavlad95 <negrea.vlad@yahoo.com>
2016-11-10 12:38:41 +0200

    show only for admins directive

---
+++ src/app/authentication/directives/show-only-for-admins.js
@@ -0,0 +1,17 @@
+function showOnlyForAdmins($animate, Authentication) {
+    return {
+    transclude: "element",
+    priority: 600,
+    link: function(scope, el, attr, ctrl, transclude) {
+      if (Authentication.isAdmin()) {
+        transclude(function(clone) {
+          $animate.enter(clone, el.parent(), el);
+        });
+      }
+    }
+    }
+}
+
+angular
+    .module("auth")
+    .directive("showOnlyForAdmins", showOnlyForAdmins);
