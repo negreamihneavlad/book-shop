936b29a5ad59aeb28c429a04e290940f533e4e9e
negreavlad95 <negrea.vlad@yahoo.com>
2016-11-10 12:38:54 +0200

    admin guard implemented

---
+++ src/app/authentication/services/admin-guard.js
@@ -0,0 +1,21 @@
+function AdminGuard($state, Authentication) {
+    return {
+        onStateChangeStart: onStateChangeStart
+    };
+
+    function onStateChangeStart(event, toState) {
+        if (toState.adminGuard && !Authentication.isAdmin()) {
+            event.preventDefault();
+
+            $state.go("login", {}, {
+                reload: true
+            });
+        }
+    }
+
+
+}
+
+angular
+    .module("auth")
+    .service("AdminGuard", AdminGuard);
