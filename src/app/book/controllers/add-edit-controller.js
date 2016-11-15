1a40d39905ddabf33a6706165245e23182c4a9f2
negreavlad95 <negrea.vlad@yahoo.com>
2016-11-15 11:06:34 +0200

    updated

--- src/app/book/controllers/add-edit-controller.js
+++ src/app/book/controllers/add-edit-controller.js
@@ -1,56 +1,44 @@
-(function() {
+function AddEditCtrl($filter, Book, getOneBook) {
 
-    angular
-        .module("bookShop")
-        .controller("AddEditCtrl", AddEditCtrl);
+    var vm = this;
 
-    function AddEditCtrl($filter, Books, getOneBook) {
+    vm.book = getOneBook || {};
+    vm.submit = submit;
 
-        var vm = this;
+    function createBook() {
+        console.log(vm.book);
+        Book.create(vm.book)
+            .then(function(response) {
+                console.log(vm.book);
+                vm.createdMessage = 'Book added successfully!';
+                console.log('created: ', response);
+            })
+            .catch(function() {
+                vm.createdMessage = 'Failed to add book!';
+            });
+    }
 
-        vm.book = getOneBook || {};
+    function updateBook() {
+        Book.update(vm.book.id, vm.book)
+            .then(function(response) {
+                vm.updateMessage = 'Book updated successfully!';
+                console.log('updated:', response);
+            })
+            .catch(function() {
+                vm.updateMessage = 'Failed to update the book!';
+            });
+    }
 
-        vm.submit = submit;
-
-
-        function createBook() {
-
-            console.log(vm.book);
-            Books.create(vm.book)
-                .then(function(response) {
-                    console.log(vm.book);
-                    vm.createdMessage = 'Book added successfully!';
-                    console.log('created: ', response);
-                })
-                .catch(function() {
-                    vm.createdMessage = 'Failed to add book!';
-                });
-
-        }
-
-        function updateBook() {
-
-            Books.update(vm.book.id, vm.book)
-                .then(function(response) {
-                    vm.updateMessage = 'Book updated successfully!';
-                    console.log('updated:', response);
-
-                })
-                .catch(function() {
-                    vm.updateMessage = 'Failed to update the book!';
-
-                });
-        }
-
-        function submit() {
-            if (vm.addBook.$valid) {
-                if (getOneBook) {
-                    updateBook();
-                } else {
-                    createBook();
-                }
+    function submit() {
+        if (vm.addBook.$valid) {
+            if (getOneBook) {
+                updateBook();
+            } else {
+                createBook();
             }
         }
     }
-
-}());
+}
+angular
+    .module("bookShop")
+    .controller("AddEditCtrl", AddEditCtrl);
