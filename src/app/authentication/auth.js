c564ea85b64cf6c7cd7898007779126278397bff
negreavlad95 <negrea.vlad@yahoo.com>
2016-11-21 14:58:18 +0200

    updated

--- src/app/authentication/auth.js
+++ src/app/authentication/auth.js
@@ -19,9 +19,33 @@ function config($httpProvider, $stateProvider) {
             parent: 'main',
             templateUrl: 'app/authentication/templates/sign-up.html',
             controller: 'SignUpCtrl as sign'
-        });
+        })
+        .state('accountEdit', {
+            url: '/account/edit',
+            parent: 'main',
+            templateUrl: 'app/authentication/templates/account-editor.html',
+            controller: 'AccountEditorCtrl as edit'
+        })
+        .state('updatePassword', {
+            url: '/account/edit/change-password',
+            parent: 'main',
+            templateUrl: 'app/authentication/templates/update-password.html',
+            controller: 'UpdatePassCtrl as update'
+        })
+        .state('forgotPassword', {
+            url: '/account/forgot-password',
+            parent: 'main',
+            templateUrl: 'app/authentication/templates/forgot-password.html',
+            controller: 'ForgotPassCtrl as forgot'
+        })
+        .state({
+            name: "resetPassword",
+            url: "/account/reset-password?email&code",
+            parent: 'main',
+            templateUrl: "app/authentication/templates/reset-password.html",
+            controller: "ResetPasswordCtrl as resetPassword"
+        })
 }
-
 angular
     .module("auth", ["ui.router", "templates"])
     .config(config);
