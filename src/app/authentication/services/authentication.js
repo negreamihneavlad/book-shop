800907580da64a7bf97721e1446c6ca817070a2a
negreavlad95 <negrea.vlad@yahoo.com>
2016-11-21 15:01:38 +0200

    comments

--- src/app/authentication/services/authentication.js
+++ src/app/authentication/services/authentication.js
@@ -17,6 +17,7 @@ function Authentication($http, $rootScope, Session, AUTH_EVENTS) {
         signUp: signUp
     };
     /**
+     *Request to log in a user
      *
      * @param email
      * @param password
@@ -27,39 +28,57 @@ function Authentication($http, $rootScope, Session, AUTH_EVENTS) {
             email: email,
             password: password
         });
-
-        return promise.then(function(response) {
-            $rootScope.$emit(AUTH_EVENTS.loggedIn, response.data);
+        return promise.then(function (response) {
             Session.create(response.headers("Authorization"), response.data);
             return response;
         });
     }
 
     /**
+     * Request to sign up a user
      *
      * @param email
      * @param password
      */
-    function signUp(email, password) {
+    function signUp(firstName, lastName, email, password) {
         var promise = $http.post("http://localhost:3000/sign-up", {
+            firstName: firstName,
+            lastName: lastName,
             email: email,
             password: password
         });
+        return promise.then(function(response){
+            return response;
+        });
     }
 
+    /**
+     * Check if user is authenticated
+     * @returns {*}
+     */
     function isAuthenticated() {
         return Session.isActive();
     }
 
+    /**
+     * Log out user
+     */
     function logout() {
         $rootScope.$emit(AUTH_EVENTS.loggedOut, {});
         Session.destroy();
     }
 
+    /**
+     * Check if user session is active
+     * @returns {*}
+     */
     function isActive() {
         return Session.isActive();
     }
 
+    /**
+     * Check if user is admin
+     */
     function isAdmin() {
         return Session.get("isAdmin");
     }
