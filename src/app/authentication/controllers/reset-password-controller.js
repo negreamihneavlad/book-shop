da80665f19c17ac4c54c18d8808785369239f839
negreavlad95 <negrea.vlad@yahoo.com>
2016-11-21 14:59:39 +0200

    reset password by token

---
+++ src/app/authentication/controllers/reset-password-controller.js
@@ -0,0 +1,49 @@
+function ResetPasswordCtrl($window, $state, $stateParams, Account, Authentication) {
+    var vm = this;
+
+    vm.credentials = {};
+    vm.resetPassword = resetPassword;
+    vm.ui = buildUI();
+
+    vm.ui.isSubmitting = true;
+    /**
+     * Resets password and log in
+     */
+    function resetPassword() {
+        if (vm.form.$valid) {
+
+            Account.resetPassword($stateParams.email, vm.credentials.newPassword, $stateParams.code)
+                .then(function () {
+                    return Authentication.login($stateParams.email, vm.credentials.newPassword);
+                })
+
+                .then(function () {
+                    $state.go("home", {}, {reload: true});
+                })
+
+                .catch(function () {
+                    $window.alert("This token is expired or invalid.");
+                })
+
+                .finally(function () {
+                    vm.ui.isSubmitting = false;
+                });
+        }
+    }
+
+
+
+}
+/**
+ * Build UI.
+ *
+ * @returns {{isSubmitting: boolean}}
+ */
+function buildUI() {
+    return {
+        isSubmitting: false
+    };
+}
+angular
+    .module("auth")
+    .controller("ResetPasswordCtrl", ResetPasswordCtrl);
