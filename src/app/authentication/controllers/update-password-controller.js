807ec0d72eb8cc946425506fa0411c489914b5ef
negreavlad95 <negrea.vlad@yahoo.com>
2016-11-21 15:00:07 +0200

    update password if logged in

---
+++ src/app/authentication/controllers/update-password-controller.js
@@ -0,0 +1,33 @@
+/**
+ *
+ * @param $state
+ * @param Account
+ * @constructor
+ */
+function UpdatePassCtrl($state, Account) {
+    var vm = this;
+    vm.updatePassword = updatePassword;
+    /**
+     * Update password when user is logged in
+     */
+    function updatePassword() {
+        if (vm.form.$valid) {
+            Account.updatePassword(vm.oldPassword, vm.newPassword)
+                .then(function (response) {
+                    vm.updateMessage = 'Password updated successfully!';
+                    $state.go("home", {}, {reload: true});
+                    console.log('updated:', response);
+                })
+                .catch(function () {
+                    vm.updateMessage = 'Old password does not match!';
+                });
+        }
+    }
+
+
+}
+
+angular
+    .module("auth")
+    .controller("UpdatePassCtrl", UpdatePassCtrl);
+
