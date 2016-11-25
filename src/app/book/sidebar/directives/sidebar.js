a0284de09ec67e4072fba0e16a1ca31b1f2397ed
negreavlad95 <negrea.vlad@yahoo.com>
2016-11-25 14:35:16 +0200

    implemented remove filters

--- src/app/book/sidebar/directives/sidebar.js
+++ src/app/book/sidebar/directives/sidebar.js
@@ -1,13 +1,25 @@
 /**
- *
+ * $stateParams
+ * $state
  * @constructor
  */
-function SidebarCtrl() {
+function SidebarCtrl($stateParams, $state) {
     var vm = this;
+    vm.removeFilter = removeFilter;
 
     vm.categories = _.uniqBy(vm.books, 'category');
     vm.publishers = _.uniqBy(vm.books, 'publisher');
     vm.authors = _.uniqBy(vm.books, 'author');
+    vm.filters = $stateParams;
+
+    function removeFilter(filterr){
+        _.forEach($stateParams,function(value,key){
+            if (value == filterr){
+                $stateParams[key] = undefined;
+            }
+        });
+        $state.go($state.current,$stateParams);
+    }
 }
 /**
  *
