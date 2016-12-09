f4b25e8e46cc7793849f8cd3387e3e93b55c57ac
negreavlad95 <negrea.vlad@yahoo.com>
2016-12-09 16:54:54 +0200

    pagination

--- src/app/book/controllers/search-books-controller.js
+++ src/app/book/controllers/search-books-controller.js
@@ -3,13 +3,35 @@
  *
  * @param searchedBooks
  * @param Page
+ * @param categories
+ * @param publishers
+ * @param authors
+ * @param length
+ * @param $state
+ * @param $stateParams
  * @constructor
  * @ngInject
  */
-function SearchCtrl(Page, searchedBooks) {
-    Page.setTitle('Book Shop-Search Books');
+function SearchCtrl($state, $stateParams, Page, searchedBooks, categories, publishers, authors, length) {
+    Page.setTitle('-Search Books');
     var vm = this;
+    vm.pagination = buildPagination();
     vm.bookList = searchedBooks;
+    vm.categories = categories;
+    vm.publishers = publishers;
+    vm.authors = authors;
+
+    //////////////////////////
+
+    function buildPagination(){
+        return {
+            totalResults : length,
+            currentPage: $stateParams.page ? $stateParams.page : 1,
+            limitPerPage: 10,
+            maxButtonNumber: 4,
+            state: $state.current.name
+        }
+    }
 }
 
 angular
