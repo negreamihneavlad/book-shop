f0cbfe12b1d0895ef8fd9fcf3c91c734165f8c9d
negreavlad95 <negrea.vlad@yahoo.com>
2016-12-19 16:17:27 +0200

    check if email is already registered

---
+++ src/app/authentication/services/existing-account.js
@@ -0,0 +1,30 @@
+/**
+ *
+ * @param $http
+ * @param ACCOUNT_API_URLS
+ * @returns {{canBeRegistered: canBeRegistered}}
+ * @constructor
+ */
+function ExistingAccount($http, ACCOUNT_API_URLS) {
+    return {
+        canBeRegistered: canBeRegistered
+    };
+
+    /**
+     * Verify whether an email can be registered.
+     *
+     * @param email
+     * @returns {*}
+     */
+    function canBeRegistered(email) {
+        return $http.get(URLTo.api(ACCOUNT_API_URLS.checkEmail), {
+            params: {
+                email: email
+            }
+        });
+    }
+}
+
+angular
+    .module("auth")
+    .service("ExistingAccount", ExistingAccount);
