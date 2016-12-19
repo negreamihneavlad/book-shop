9e9db5207822533903e73dd88a9097b70a8971b1
negreavlad95 <negrea.vlad@yahoo.com>
2016-12-19 16:15:48 +0200

    page name

--- src/app/authentication/auth.js
+++ src/app/authentication/auth.js
@@ -13,38 +13,56 @@ function config($httpProvider, $stateProvider) {
             url: '/login',
             parent: 'main',
             templateUrl: 'app/authentication/templates/login.html',
-            controller: 'LogInCtrl as login'
+            controller: 'LogInCtrl as login',
+            data: {
+                pageTitle: 'Book Shop - Login'
+            }
         })
         .state('sign', {
             url: '/sign-up',
             parent: 'main',
             templateUrl: 'app/authentication/templates/sign-up.html',
-            controller: 'SignUpCtrl as sign'
+            controller: 'SignUpCtrl as signUp',
+            data: {
+                pageTitle: 'Book Shop - Sign Up'
+            }
         })
         .state('accountEdit', {
             url: '/account/edit',
             parent: 'main',
             templateUrl: 'app/authentication/templates/account-editor.html',
-            controller: 'AccountEditorCtrl as edit'
+            controller: 'AccountEditorCtrl as edit',
+            data: {
+                pageTitle: 'Book Shop - Edit Account'
+            }
         })
         .state('updatePassword', {
             url: '/account/edit/change-password',
             parent: 'main',
             templateUrl: 'app/authentication/templates/update-password.html',
-            controller: 'UpdatePassCtrl as update'
+            controller: 'UpdatePassCtrl as update',
+            data: {
+                pageTitle: 'Book Shop - Update Password'
+            }
         })
         .state('forgotPassword', {
             url: '/account/forgot-password',
             parent: 'main',
             templateUrl: 'app/authentication/templates/forgot-password.html',
-            controller: 'ForgotPassCtrl as forgot'
+            controller: 'ForgotPassCtrl as forgot',
+            data: {
+                pageTitle: 'Book Shop - Forgot Password'
+            }
         })
         .state({
             name: "resetPassword",
             url: "/account/reset-password?email&code",
             parent: 'main',
             templateUrl: "app/authentication/templates/reset-password.html",
-            controller: "ResetPasswordCtrl as resetPassword"
+            controller: "ResetPasswordCtrl as resetPassword",
+            data: {
+                pageTitle: 'Book Shop - Reset Password'
+            }
         });
 }
 angular
