b09839f53becd27e334b7e45fe218cbf710a345a
negreavlad95 <negrea.vlad@yahoo.com>
2016-12-19 16:19:11 +0200

    page title directive

---
+++ src/app/layout/directives/page-title.js
@@ -0,0 +1,30 @@
+/**
+ * Page title.
+ *
+ * @returns {{title: title, setTitle: setTitle}}
+ * @constructor
+ */
+function updateTitle($rootScope, $timeout) {
+    return {
+        link: function(scope, element) {
+
+            var listener = function(event, toState) {
+
+                var title = 'Book Shop';
+                if (toState.data && toState.data.pageTitle) {
+                    title = toState.data.pageTitle;
+                }
+
+                $timeout(function() {
+                    element.text(title);
+                }, 0, false);
+            };
+
+            $rootScope.$on('$stateChangeSuccess', listener);
+        }
+    };
+}
+
+angular
+    .module("app")
+    .directive("updateTitle", updateTitle);
