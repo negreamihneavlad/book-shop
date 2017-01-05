dd9d3fdfcfa54055ef6feba5e013910090ee2ca7
negreavlad95 <negrea.vlad@yahoo.com>
2017-01-05 16:45:06 +0200

    add to cart

---
+++ src/app/order/directives/add-to-cart.js
@@ -0,0 +1,41 @@
+/**
+ * Add item controller
+ *
+ * @param Cart
+ * @param Session
+ * @constructor
+ */
+function AddToCartCtrl(Cart, Session) {
+    var vm = this;
+    vm.user = Session.getData();
+    vm.addItemToCart = addItemToCart;
+
+    //////////////////////////////
+    /**
+     * Add item to cart
+     */
+    function addItemToCart() {
+        Cart.addItem(vm.user.id, vm.bookId, 1);
+    }
+}
+/**
+ * Add item directive
+ *
+ * @returns {{templateUrl: string, replace: boolean, bindToController: boolean, scope: {bookId: string}, controller: AddToCartCtrl, controllerAs: string}}
+ */
+function addToCart() {
+    return {
+        templateUrl: "app/order/templates/add-to-cart.html",
+        replace: true,
+        bindToController: true,
+        scope: {
+            bookId: '='
+        },
+        controller: AddToCartCtrl,
+        controllerAs: "addToCart"
+    };
+}
+
+angular
+    .module("order")
+    .directive("addToCart", addToCart);
